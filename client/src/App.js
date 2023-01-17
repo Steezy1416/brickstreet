553f79ae41f6f07b3766351993ac72bfcc490f52
FDeaquino <fidel.deaquino@gmail.com>
2023-01-17 16:16:14 -0600

    updated hero photo and added styling, created footer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './components/Hero';
 import Nav from './components/Nav';
 import Login from './components/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <main>
       <Hero></Hero>
       </main>
-
+      <Footer></Footer>
     </div>
   )
 }
--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -5,12 +5,12 @@ function Hero() {
     return (
         <section className="py-4 py-xl-5">
             <div className="container">
-                <div className="hero-container-custom-css border rounded border-0 d-flex flex-column justify-content-center align-items-center p-4 py-5">
-                    <div className="row">
+                <div className="hero-container-custom-css border rounded border-0 d-flex flex-column justify-content-center align-items-center">
+                    <div className="row p-4 py-5 hero-items-styles">
                         <div className="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
                             <div>
-                                <h1 className="text-uppercase fw-bold mb-3">Rediscover the fun of building with Brick Street</h1>
-                                <p className="mb-4">Buy and Sell Legos<br />Connect with Lego Enthusiasts</p>
+                                <h1 className="hero-text-styles text-uppercase fw-bold mb-3">Rediscover the fun of building with Brick Street</h1>
+                                <p className="hero-text-styles mb-4">Buy and Sell Legos<br />Connect with Lego Enthusiasts</p>
                                 
                                 <button className="btn fs-5 me-2 py-2 px-4 btn-moving-gradient btn-moving-gradient--black">
                                      Log In
