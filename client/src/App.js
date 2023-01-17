a7887ac92d53af0ba5f6805b452b91f837e66984
FDeaquino <fidel.deaquino@gmail.com>
2023-01-17 12:50:37 -0600

    created hero and login page components, added bootstrap and css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Hero from './components/Hero';
 import Nav from './components/Nav';
-
+import Login from './components/Login';
 
 function App() {
   return (
     <div>
       <Nav></Nav>
       <main>
-
+      <Hero></Hero>
       </main>
 
     </div>
---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+// TODO: update placeholder links in nav bar
+
+function Hero() {
+    return (
+        <section className="py-4 py-xl-5">
+            <div className="container">
+                <div className="hero-container-custom-css border rounded border-0 d-flex flex-column justify-content-center align-items-center p-4 py-5">
+                    <div className="row">
+                        <div className="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
+                            <div>
+                                <h1 className="text-uppercase fw-bold mb-3">Rediscover the fun of building with Brick Street</h1>
+                                <p className="mb-4">Buy and Sell Legos<br />Connect with Lego Enthusiasts</p>
+                                
+                                <button className="btn fs-5 me-2 py-2 px-4 btn-moving-gradient btn-moving-gradient--black">
+                                     Log In
+                                </button>
+                                <button className="btn fs-5 me-2 py-2 px-4 btn-moving-gradient btn-moving-gradient--orange">
+                                     Sign Up 
+                                </button>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
