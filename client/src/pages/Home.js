764cd3d8816db8ed6f28e559fbd7d31035358427
amaragh <mercer.alecia@gmail.com>
2023-01-18 22:05:49 -0600

    initial framework for home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+const Home = () => {
+    return (
+        <main>
+            <p>This is the HOME page</p>
+        </main>
+    )
+}
+
+export default Home;
