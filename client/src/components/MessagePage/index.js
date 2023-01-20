f4e9c131dd8d63719ffeafefdfc3816000302b07
alexfortin <fortin.alex14@gmail.com>
2023-01-20 16:08:00 -0600

    added socket.io to server to js and it connects to the client, also gave clent proxy

--- client/src/components/MessagePage/index.js
+++ client/src/components/MessagePage/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
-// TODO: 
+
+const { io } = require("socket.io-client")
+const socket = io("http://localhost:3000")
+socket.on("connect", () => {
+    console.log(socket.id)
+})
 
 function MessagePage() {
     return (
