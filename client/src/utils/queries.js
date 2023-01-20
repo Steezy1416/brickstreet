8360ae8b4629ffe433eed50626dee1af90194c74
amaragh <mercer.alecia@gmail.com>
2023-01-19 20:29:11 -0600

    write queries for get single post and get user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -14,3 +14,41 @@ export const QUERY_POSTS = gql`
         createdAt
     }
 `
+export const QUERY_POST = gql`
+    query getPost($id:ID!) {
+        getPost(id:$id) {
+            id
+            user
+            topBidder
+            postImage
+            title
+            description
+            bidPrice
+            categories
+            availability
+            createdAt
+        }
+    }
+`;
+
+export const QUERY_USER = gql`
+    query getUser($id:ID!) {
+        getUser(id:$id){
+            id
+            name
+            posts {
+                id
+                user
+                topBidder
+                postImage
+                title
+                description
+                bidPrice
+                categories
+                availability
+                createdAt
+            }
+            profilePicture
+        }
+    }
+`
