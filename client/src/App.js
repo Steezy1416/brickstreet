ed854fc66f38eb0ea09bc854714c8cc85c6b9ff1
graslowsnail <graslowsnail@gmail.,com>
2023-01-21 14:54:59 -0600

    finished styling for add post form.

--- client/src/App.js
+++ client/src/App.js
@@ -1,110 +1,88 @@
 // JT
-import React from 'react';
-import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
-import Signup from './pages/Register';
-import Home from './pages/Home';
-import About from './pages/About';
-import Post from './pages/Post'
-import AddPost from './pages/AddPost.js';
+import Signup from "./pages/Register";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Post from "./pages/Post";
+import AddPost from "./pages/AddPost.js";
 // import FAQ from './pages/FAQ';
 // import Contact from './pages/Contact';
-import UserDashboard from './pages/UserDashboard';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-import Login from './components/Login';
-import MessagePage from './components/MessagePage';
-import Footer from './components/Footer';
-
+import UserDashboard from "./pages/UserDashboard";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Login from "./components/Login";
+import MessagePage from "./components/MessagePage";
+import Footer from "./components/Footer";
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-    const token = localStorage.getItem('id_token');
-    return {
-        headers: {
-            ...headers,
-            authorization: token ? `Bearer ${token}` : '',
-        },
-    };
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
 });
 
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
 });
 
 function App() {
-    return (
-        
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <Nav />
-                    <div className="container">
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={<Hero />}
-                            />
-                            <Route
-                                path="/home"
-                                element={<Home />}
-                            />
-                            <Route
-                                path="/about"
-                                element={<About />}
-                            />
-                            <Route
-                                path="/messages"
-                                element={<MessagePage />}
-                            />
-                            <Route
-                                path="/post"
-                                element={<AddPost />}
-                            />
-                            <Route
-                                path="/dashboard"
-                                element={<UserDashboard />}
-                            />
-                            <Route
-                                path="/login"
-                                element={<Login />}
-                            />
-                            <Route
-                                path="/register"
-                                element={<Signup />}
-                            />
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Nav />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Hero />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/messages" element={<MessagePage />} />
+              <Route path="/post" element={<AddPost />} />
+              <Route path="/dashboard" element={<UserDashboard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Signup />} />
 
-                            {/* TODO: Import the Post page/component once it's finished and uncomment the code below */}
-                            {/* <Route
+              {/* TODO: Import the Post page/component once it's finished and uncomment the code below */}
+              {/* <Route
                                 path="/post"
                                 element={<Post />}
                             /> */}
 
-                            {/* TODO: Import the Account page/component once it's finished and uncomment the code below */}
-                            {/* <Route
+              {/* TODO: Import the Account page/component once it's finished and uncomment the code below */}
+              {/* <Route
                                 path="/account"
                                 element={<Account />}
                             /> */}
 
-                            {/* TODO: decide if we want to add a no match feature, like in deep-thoughts */}
-                            {/* <Route
+              {/* TODO: decide if we want to add a no match feature, like in deep-thoughts */}
+              {/* <Route
                                 path="*"
                                 element={<NoMatch />}
                             /> */}
-                        </Routes>
-                    </div>
-                    <Footer />
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
 }
 
 export default App;
--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect} from 'react';
-import Select from 'react-select';
-
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
 
 const AddPost = () => {
-
-    const [values, setValues] = useState({
+  const [values, setValues] = useState({
     name: "",
     //userId: _id,
     description: "",
@@ -13,110 +11,115 @@ const AddPost = () => {
     formData: new FormData(),
   });
 
+  const { name, description, price, category, formData, type } = values;
 
-    const {
-        name,
-        description,
-        price,
-        category,
-        formData,
-        type
-    } = values;
+  const handleChange = (name) => (event) => {
+    const value = event.target.value;
+    formData.set(name, value);
+    setValues({ ...values, [name]: value });
+  };
 
+  const options = [
+    { value: "chocolate", label: "Chocolate" },
+    { value: "strawberry", label: "Strawberry" },
+    { value: "vanilla", label: "Vanilla" },
+  ];
 
-    const handleChange = (name) => (event) => {
-        const value = event.target.value;
-        formData.set(name, value);
-        setValues({ ...values, [name]: value });
-    };
+  return (
+    <section className="position-relative py-4 py-xl-5">
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-md-8 col-xl-6 text-center mx-auto">
+            <h2>Add Post</h2>
+          </div>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-md-6 col-xl-4">
+            <div className="card mb-5">
+              <div className="card-body d-flex flex-column align-items-center">
+                <form className="text-center" method="post">
+                  <div className="mb-3">
+                    {/* ITEM NAME */}
+                    <div className="container">
+                      {/* <label className="">Item name</label> */}
 
-    const options = [
-      { value: 'chocolate', label: 'Chocolate' },
-      { value: 'strawberry', label: 'Strawberry' },
-      { value: 'vanilla', label: 'Vanilla' }
-    ]
+                      <div className="row">
+                        <label className="mb-2">Item name</label>
+                        <input
+                          type="text"
+                          onChange={handleChange("name")}
+                          value={name}
+                          name="name"
+                          id="name"
+                          className="col-12 mb-3"
+                        />
+                        <label className="mb-2">Description</label>
+                        <textarea
+                          id="description"
+                          onChange={handleChange("description")}
+                          value={description}
+                          name="description"
+                          rows={2}
+                          className="col-12 mb-3"
+                          defaultValue={""}
+                        />
+                        <label className="mb-2">
+                          Choose a product category
+                        </label>
+                        <Select
+                          defaultValue={[options[2]]}
+                          isMulti
+                          name="colors"
+                          options={options}
+                          className="basic-multi-select col-12 mb-3"
+                          classNamePrefix="select"
+                        />
+                        <div class="input-group mb-3">
+                          <input
+                            type="file"
+                            class="form-control"
+                            id="inputGroupFile02"
+                          ></input>
+                          <label
+                            class="input-group-text"
+                            for="inputGroupFile02"
+                          >
+                            Upload
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <div className="mb-3">
+                    
+                    <label className="container">Description</label>
+                    <div className="row">
+                      <textarea
+                        id="description"
+                        onChange={handleChange("description")}
+                        value={description}
+                        name="description"
+                        rows={2}
+                        className="col-12"
+                        defaultValue={""}
+                      />
+                    </div>
+                    <p className="">Write a few sentences about the product.</p>
+                  </div> */}
 
-    return (
-        <div className=''>
-                <div className='col-md-8 col-xl-6 text-center mx-auto'>
-                    <h2 className="">Add Your lego set!</h2>
-        <p className="w-lg-50">List your lego set for sale!</p>
-                </div>
-
-
-        <div className=''>
-<form className=' text-center w-50 ' onSubmit=''>
-
-            {/* CATEGORY */}
-            <Select
-            defaultValue={[options[2]]}
-            isMulti
-            name="colors"
-            options={options}
-            className="basic-multi-select"
-            classNamePrefix="select"
-          />
-
-
-        {/* ITEM NAME */}
-        <div className="">
-              <label className="">
-                Item name
-              </label>
-
-              <div className="">
-                <input
-                  type="text"
-                  onChange={handleChange("name")}
-                  value={name}
-                  name="name"
-                  id="name"
-                  className=""
-                />
+                  <div className="mb-3">
+                    <button className="btn btn-moving-gradient btn-dark">
+                      List Product
+                    </button>
+                  </div>
+                </form>
               </div>
             </div>
-
-
-        {/* DISCRIPTION BOX*/}
-        <div className="">
-              <label className="">
-                Description
-              </label>
-              <div className="">
-                <textarea
-                  id="description"
-                  onChange={handleChange("description")}
-                  value={description}
-                  name="description"
-                  rows={2}
-                  className=""
-                  defaultValue={""}
-                />
-              </div>
-              <p className="">
-                Write a few sentences about the product.
-              </p>
+          </div>
         </div>
-
-        {/* PICTURE UPLOAD*/}
-        <div class="input-group mb-3">
-          <input type="file" class="form-control" id="inputGroupFile02"></input>
-          <label class="input-group-text" for="inputGroupFile02">Upload</label>
-        </div>
-
-
-        {/*BUTTON*/}
-        <div className="">
-          <button className="btn btn-moving-gradient btn-dark">
-            List Product
-          </button>
-        </div>
-
-
-        </form>
-        </div>
-        </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default AddPost;
