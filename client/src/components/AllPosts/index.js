d70648d94d4036fb9565893f6208b2c9018bb709
amaragh <mercer.alecia@gmail.com>
2023-01-21 19:13:47 -0600

    add availability badge to post

--- client/src/components/AllPosts/index.js
+++ client/src/components/AllPosts/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import { CardGroup, Card, CardBody, CardTitle, CardSubtitle, CardText, CardImg,CardLink } from 'reactstrap'
+import { CardGroup, Card, CardBody, CardTitle, CardSubtitle, CardText, CardImg, CardLink, Badge } from 'reactstrap'
 import { useQuery } from "@apollo/client";
 
 import { useParams } from "react-router-dom";
@@ -9,13 +9,18 @@ const AllPosts = ({ posts }) => {
 
     return (
         <div>
-            <CardGroup className='m-5'>
+            <CardGroup className='m-3'>
                 {posts && posts.map(post => (
+                    
                     <div key={post.id}>
                         <Card style={{ width: '15rem' }} className='m-2'>
                             <CardBody>
                                 <CardTitle tag='h6'>{post.title}</CardTitle>
-                                <CardText>Bid: {post.bidPrice}</CardText>
+                                <CardText>
+                                    Bid: {post.bidPrice}
+                                    {' '}
+                                    <Badge color={post.availability === 'available' ? "success":"secondary"} style={{ "opacity": "0.9" }} pill className="available-post">{post.availability.charAt(0).toUpperCase() + post.availability.substr(1).toLowerCase()}</Badge>
+                                </CardText>
                             </CardBody>
                             <CardImg src={`${post.postImage}`} alt={`${post.title}`} />
                             <CardBody>
