ef421963e74eaa1293fc0ad756b2169c7355dff0
amaragh <mercer.alecia@gmail.com>
2023-01-21 18:24:05 -0600

    style categories on home pahe as inline list

--- client/src/components/Categories/index.js
+++ client/src/components/Categories/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { List, ListInlineItem } from 'reactstrap'
+
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
-import { Link } from 'react-router-dom';
+
 
 
 const Categories = () => {
@@ -12,22 +15,17 @@ const Categories = () => {
             <h4>Categories</h4>
             <List type="inline">
                 <ListInlineItem>
-                    Lorem ipsum
+                    <Link to={`/home/all`}>All</Link>
                 </ListInlineItem>
-                <ListInlineItem>
-                    Phasellus iaculis
-                </ListInlineItem>
-                <ListInlineItem>
-                    Nulla volutpat
-                </ListInlineItem>
+                {categories.map(category => (
+                    <ListInlineItem key={category.id}>
+                        <Link onClick={() => { console.log(category.categoryName) }} to={`/home/${category.categoryName}`}>{category.categoryName}</Link>
+                    </ListInlineItem>
+                ))}
             </List>
-            <Link to={`/home/all`}>All</Link>
 
-            {categories.map(category => (
-                <div key={category.id}>
-                    <Link onClick={() => { console.log(category.categoryName) }} to={`/home/${category.categoryName}`}>{category.categoryName}</Link>
-                </div>
-            ))}
+
+
         </div>
     )
 }
