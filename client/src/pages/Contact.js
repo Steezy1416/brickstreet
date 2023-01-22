d19ba647d0c73a893c63dc94c031f3cd6d90abf1
graslowsnail <graslowsnail@gmail.,com>
2023-01-22 10:43:45 -0600

    added content and styling to the Contact Page. Added finishing touches to the Privacy Policy page. Added tab style to the css sheet.

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,7 +2,66 @@ import React from 'react';
 
 const Contact = () => (
     <div>
+        <h2>Contact Us!</h2>
+            <p>At Brick Street, we value your feedback and are dedicated to providing you with the best service possible. If you have any questions, comments, or concerns, please do not hesitate to contact us.</p>
+    <dl>
+    <dd>-Phone: 832-112-5677</dd>
+    <dd>-Email: info@BrickStreet.com</dd>
+    <dd>-Address: 123 Main St, Houston TX 77475</dd>
+</dl>
+    <h6> Or use the contact from below:</h6>
+        <div className="row d-flex ">
+          <div className="col-md-5 col-xl-4">
+            <div className="card mb-5">
+              <div className="card-body d-flex flex-column align-items-center">
+                <form className="text-center" method="post">
+                  <div className="mb-3">
+                    <div className="container">
 
+                      <div className="row">
+                        <label className="mb-2">Name</label>
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          className="col-12 mb-3"
+                        />
+                            <label className="mb-2">Email</label>
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          className="col-12 mb-3"
+                        />
+                        <label className="mb-2">Phone</label>
+                        <input
+                          type="text"
+                          name="name"
+                          id="name"
+                          className="col-12 mb-3"
+                        />
+
+                        <label className="mb-2">Message</label>
+                        <textarea
+                          className="col-12 mb-3"
+                          defaultValue={""}
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="mb-3">
+                    <button className="btn btn-moving-gradient btn-dark">
+                      Submit
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+    <h5> Thank you for choosing Brick Street!</h5>
     </div>
 );
 
--- client/src/pages/PrivacyPolicy.js
+++ client/src/pages/PrivacyPolicy.js
@@ -4,7 +4,6 @@ const PrivacyPolicy = () => (
 
 <div className="">
                     <h2 className="">Brick Street Privacy Policy</h2>
-                    <div className="">
     <p className=''>
         At Brick Street, we understand the importance of protecting your personal information. We are committed to maintaining the privacy and security of your data in accordance with this Privacy Policy. This policy explains the types of information we collect, how we use it, and the steps we take to protect it.
 <br />
@@ -15,18 +14,16 @@ const PrivacyPolicy = () => (
 <dd>-Financial information, such as your credit card or banking information.</dd>
 <dd>-Demographic information, such as your age, gender, and interests.</dd>
 <dd>-Technical information, such as your IP address, browser type, and device type.</dd>
-<dd>-Information about your usage of our services, such as the pages you visit and the content you view.</dd> <br />
+<dd>-Information about your usage of our services, such as the pages you visit and the content you view.</dd>
 </dl>
-<br />
     <h5>How We Use Your Information: <br/></h5>
 <dl>
 <dd>-To provide you with the products and services you have requested.</dd>
 <dd>-To communicate with you regarding your account or transactions with us.</dd>
 <dd>-To improve our products and services.</dd>
 <dd>-To personalize your experience on our website or app.</dd>
-<dd>-To send you promotional offers and information about our products and services.</dd> <br />
+<dd>-To send you promotional offers and information about our products and services.</dd>
     </dl>
-<br />
     <h5>Sharing of Information:<br /></h5>
 
 -We will not share your personal information with any third party without your consent, except as required by law or to protect our rights.
@@ -38,12 +35,11 @@ const PrivacyPolicy = () => (
 <dd>-We use industry-standard encryption technologies to protect your sensitive information during transmission.</dd>
 <dd>-We regularly review and update our security protocols to ensure the protection of your personal information.</dd>
     </dl>
-<br />
     <h5>Changes to This Privacy Policy:<br /></h5>
 
     <dl>
     <dd>-We may update this Privacy Policy from time to time to reflect changes in our practices or legal requirements. </dd>
-    <dd>-We will notify you of any changes by posting the updated policy on our website.</dd><br />
+    <dd>-We will notify you of any changes by posting the updated policy on our website.</dd>
     </dl>
     <h5>Contact Us:<br /> </h5>
 
@@ -54,7 +50,6 @@ If you have any questions or concerns about our Privacy Policy or the way we han
 By using the Brick Street website or services, you agree to the terms of this Privacy Policy. If you do not agree with these terms, please do not use our website or services.
  </p>
         </div>
-    </div> 
 
 );
 
