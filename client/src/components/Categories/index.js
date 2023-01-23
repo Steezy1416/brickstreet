becc1b523a91af07e0ee472762b93bb78b891262
amaragh <mercer.alecia@gmail.com>
2023-01-22 20:27:37 -0600

    merge Categories component

---
+++ client/src/components/Categories/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { List, ListInlineItem } from 'reactstrap'
+
+import { useQuery } from '@apollo/client';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+
+
+
+const Categories = () => {
+    const { loading, data } = useQuery(QUERY_CATEGORIES);
+    const categories = data?.getCategories || [];
+    return (
+        <div>
+            <h4>Categories</h4>
+            <List type="inline">
+                <ListInlineItem>
+                    <Link to={`/home/all`}>All</Link>
+                </ListInlineItem>
+                {categories.map(category => (
+                    <ListInlineItem key={category.id}>
+                        <Link to={`/home/${category.categoryName}`}>{category.categoryName}</Link>
+                    </ListInlineItem>
+                ))}
+            </List>
+
+
+
+        </div>
+    )
+}
+
+export default Categories;
