fee5b66fb93cda80b051e8546fbaf09909c2135c
amaragh <mercer.alecia@gmail.com>
2023-01-26 16:46:17 -0600

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -43,7 +43,14 @@ io.on("connection", (socket) => {
 })
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    // Express will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
 
 const startServer = async () => {
