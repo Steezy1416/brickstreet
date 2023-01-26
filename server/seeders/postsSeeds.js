b2ff47ec2cd4703058f572d7c2061d7da0c12f89
amaragh <mercer.alecia@gmail.com>
2023-01-25 21:42:57 -0600

    add photos

--- server/seeders/postsSeeds.js
+++ server/seeders/postsSeeds.js
@@ -1,49 +1,79 @@
 const Post = require('../models/Post');
-const {eiffelTowerPhoto}= require('./images')
+const {
+    infinityGauntletPhoto,
+    lionDanceGuyPhoto,
+    seinfeldPhoto,
+    colorsPhoto,
+    racecarPhoto,
+    bathroomPhoto,
+    bouquetPhoto,
+    tajMahalPhoto
+} = require('./images')
 
 const postData = [
     {
-        title: 'Diagon Alley',
-        description: 'lorem ipsum lorem ipsum',
-        bidPrice: 100,
+        title: 'Polyjuice Potion Mistake',
+        description: 'No missing pieces. Includes golden Harry Potter.',
+        bidPrice: 70,
         availability: 'available',
-        categoryIndex: 6
+        categoryIndex: 6,
+        postImage: bathroomPhoto
     },
     {
-        title: 'Eiffel Tower',
+        title: 'Taj Mahal',
         description: 'lorem ipsum lorem ipsum',
-        bidPrice: 500,
+        bidPrice: 100,
         availability: 'available',
         categoryIndex: 4,
-        postImage: eiffelTowerPhoto
-        },
+        postImage: tajMahalPhoto
+    },
     {
-        title: "Ariel's Underwater Palace",
-        description: 'lorem ipsum lorem ipsum',
+        title: "Everyone Is Awesome",
+        description: 'No missing pieces',
         bidPrice: 300,
         availability: 'available',
-        categoryIndex: 7
+        categoryIndex: 0,
+        postImage: colorsPhoto
     },
     {
-        title: 'Millennium Falcon',
+        title: 'Flower Bouquet',
         description: 'lorem ipsum lorem ipsum',
         bidPrice: 100,
         availability: 'available',
-        categoryIndex: 8
+        categoryIndex: 3,
+        postImage: bouquetPhoto
     },
     {
-        title: 'Republic Gunship',
+        title: 'Seinfeld',
         description: 'lorem ipsum lorem ipsum',
         bidPrice: 100,
         availability: 'available',
-        categoryIndex: 9
+        categoryIndex: 15,
+        postImage: seinfeldPhoto
     },
     {
-        title: 'Pyramid',
+        title: 'Infinity Gauntlet',
         description: 'lorem ipsum lorem ipsum',
         bidPrice: 100,
         availability: 'available',
-        categoryIndex: [5,]
+        categoryIndex: 5,
+        postImage: infinityGauntletPhoto
+    },
+    {
+        title: 'Racecar',
+        description: 'lorem ipsum lorem ipsum',
+        bidPrice: 4,
+        availability: 'available',
+        categoryIndex: 10,
+        postImage: racecarPhoto
+    },
+    {
+        title: 'Lion Dance Guy',
+        description: 'lorem ipsum lorem ipsum',
+        bidPrice: 10,
+        availability: 'available',
+        categoryIndex: 14,
+        postImage: lionDanceGuyPhoto
     }
 ]
 
@@ -54,16 +84,16 @@ const seedPosts = (userIds, categoryIds) => {
             description: post.description,
             bidPrice: post.bidPrice,
             availability: post.availability,
-            userId: userIds[Math.floor(Math.random()*userIds.length)],
+            userId: userIds[Math.floor(Math.random() * userIds.length)],
             categoryIds: [categoryIds[post.categoryIndex]],
             createdAt: new Date(),
             postImage: post.postImage
         }
     })
     return Post.insertMany(newPostData)
-    .then(posts => {
-        return posts;
-    })
+        .then(posts => {
+            return posts;
+        })
 }
 
 module.exports = seedPosts;
