e4d522d83afc0bf66077b8f085b338e5d06e7890
FDeaquino <fidel.deaquino@gmail.com>
2023-01-26 13:29:18 -0600

    updated bootstrap styles for Add Post and made formatting edits to Login/index.js

--- client/src/pages/AddPost.js
+++ client/src/pages/AddPost.js
@@ -20,9 +20,22 @@ const AddPost = () => {
   };
 
   const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
+    { value: "Classic", label: "Classic" },
+    { value: "BrickHeadz", label: "BrickHeadz" },
+    { value: "Minifigures", label: "Minifigures" },
+    { value: "Flowers", label: "Flowers" },
+    { value: "Architecture", label: "Architecture" },
+    { value: "Marvel", label: "Marvel" },
+    { value: "Harry Potter", label: "Harry Potter" },
+    { value: "Disney", label: "Disney" },
+    { value: "Star Wars", label: "Star Wars" },
+    { value: "Avatar", label: "Avatar" },
+    { value: "Cars", label: "Cars" },
+    { value: "Creator", label: "Creator" },
+    { value: "City", label: "City" },
+    { value: "Ideas", label: "Ideas" },
+    { value: "Chinese New Year", label: "Chinese New Year" },
+    { value: "TV Show", label: "TV Show" }
   ];
 
   return (
@@ -30,85 +43,50 @@ const AddPost = () => {
       <div className="container">
         <div className="row mb-5">
           <div className="col-md-8 col-xl-6 text-center mx-auto">
-            <h2>Add Post</h2>
+            <h2 className='msg-pg-title'>Add Post</h2>
+            <p className="w-lg-50 msg-body">Fill out the form to add your item.</p>
           </div>
         </div>
         <div className="row d-flex justify-content-center">
           <div className="col-md-6 col-xl-4">
             <div className="card mb-5">
               <div className="card-body d-flex flex-column align-items-center">
+                <div className="bs-icon-xl bs-icon-circle bs-icon my-3"><svg xmlns="http://www.w3.org/2000/svg" width="5em" height="5em" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                  <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                </svg></div>
                 <form className="text-center" method="post">
                   <div className="mb-3">
-                    {/* ITEM NAME */}
-                    <div className="container">
-                      {/* <label className="">Item name</label> */}
-
-                      <div className="row">
-                        <label className="mb-2">Item name</label>
-                        <input
-                          type="text"
-                          onChange={handleChange("name")}
-                          value={name}
-                          name="name"
-                          id="name"
-                          className="col-12 mb-3"
-                        />
-                        <label className="mb-2">Description</label>
-                        <textarea
-                          id="description"
-                          onChange={handleChange("description")}
-                          value={description}
-                          name="description"
-                          rows={2}
-                          className="col-12 mb-3"
-                          defaultValue={""}
-                        />
-                        <label className="mb-2">
-                          Choose a product category
-                        </label>
-                        <Select
-                          defaultValue={[options[2]]}
-                          isMulti
-                          name="colors"
-                          options={options}
-                          className="basic-multi-select col-12 mb-3"
-                          classNamePrefix="select"
-                        />
-                        <div class="input-group mb-3">
-                          <input
-                            type="file"
-                            class="form-control"
-                            id="inputGroupFile02"
-                          ></input>
-                          <label
-                            class="input-group-text"
-                            for="inputGroupFile02"
-                          >
-                            Upload
-                          </label>
-                        </div>
-                      </div>
-                    </div>
+                    <input className="form-control" type="text" name="itemName" placeholder="Item name" />
                   </div>
-                  {/* <div className="mb-3">
-                    
-                    <label className="container">Description</label>
-                    <div className="row">
-                      <textarea
-                        id="description"
-                        onChange={handleChange("description")}
-                        value={description}
-                        name="description"
-                        rows={2}
-                        className="col-12"
-                        defaultValue={""}
-                      />
-                    </div>
-                    <p className="">Write a few sentences about the product.</p>
-                  </div> */}
+                  <textarea id="itemDescription" class="mb-3 form-control" name="itemDescription" rows="6" placeholder="Item Description"></textarea>
+                  <label className="mb-3">
+                    Choose a product category
+                  </label>
+                  <Select
+                    // defaultValue={[options[2]]}
+                    isMulti
+                    name="colors"
+                    options={options}
+                    className="basic-multi-select col-12 mb-3"
+                    classNamePrefix="select"
+                  />
+                  <div class="input-group mb-3">
+                    <input
+                      type="file"
+                      class="form-control"
+                      id="inputGroupFile02"
+                    ></input>
+                    <label
+                      class="input-group-text"
+                      for="inputGroupFile02"
+                    >
+                      Upload
+                    </label>
+                  </div>
 
                   <div className="mb-3">
-                    <button className="btn btn-moving-gradient btn-dark">
+                    <button className="btn btn-moving-gradient btn-dark d-block w-100" type="submit">
                       List Product
                     </button>
                   </div>
