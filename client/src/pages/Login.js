7769955faf314151979f7e851529778aa98bd54e
amaragh <mercer.alecia@gmail.com>
2023-01-26 15:57:10 -0600

    deploy to heroku

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -12,7 +12,7 @@ const navigate = useNavigate();
  const [password, setPassword] = useState("")
 
 const formhandler= async () =>{
-    const response = await fetch('http://localhost:5000/api/login', {
+    const response = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
